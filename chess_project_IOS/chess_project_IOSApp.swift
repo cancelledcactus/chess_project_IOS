2ff79d8a87462d98d484545ca2d32120375f609a
Devin <lindevin0701@outlook.com>
2026-03-18 12:42:08 -0400

    Initial Commit

---
+++ chess_project_IOS/chess_project_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  chess_project_IOSApp.swift
+//  chess_project_IOS
+//
+//  Created by NYCDOE on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct chess_project_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chess_project_IOSTests/chess_project_IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  chess_project_IOSTests.swift
+//  chess_project_IOSTests
+//
+//  Created by NYCDOE on 3/18/26.
+//
+
+import Testing
+@testable import chess_project_IOS
+
+struct chess_project_IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
