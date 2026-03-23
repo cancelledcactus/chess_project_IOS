d215dc836a741e22a82a5533aafae531e24837e8
men420-nergo <christopherm893@nycstudents.net>
2026-03-23 12:38:11 -0400

    hh

--- chess_project_IOS/ContentView.swift
+++ chess_project_IOS/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, test world!nigger")
+            Text("Hello, test world!")
             
         }
         .padding()
