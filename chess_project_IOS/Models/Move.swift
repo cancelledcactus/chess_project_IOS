683336b75c6529467d4dad046cbf33d002f67ee0
men420-nergo <christopherm893@nycstudents.net>
2026-03-24 12:43:19 -0400

    mm

---
+++ chess_project_IOS/Models/Move.swift
@@ -0,0 +1,18 @@
+//
+//  Move.swift
+//  chess_project_IOS
+//
+//  Created by NYCDOE on 3/24/26.
+//
+
+import SwiftUI
+
+struct Move: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Move()
+}
