5cb36311dc8b02eb61d09dcaf031dc5bf0cc6c05
men420-nergo <christopherm893@nycstudents.net>
2026-03-24 12:16:57 -0400

    Chess assets

--- chess_project_IOS/ContentView.swift
+++ chess_project_IOS/ContentView.swift
@@ -1,22 +1,19 @@
-//
 //  ContentView.swift
 //  chess_project_IOS
-//
 //  Created by NYCDOE on 3/18/26.
-//
+
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, test world!")
+           
+            
+            
             
         }
-        .padding()
+     
     }
 }
 
