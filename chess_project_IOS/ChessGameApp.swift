434194b48d25e8b0676b0061e97c8cfad112fa0a
men420-nergo <christopherm893@nycstudents.net>
2026-03-30 12:24:52 -0400

    nn
    nn

---
+++ chess_project_IOS/ChessGameApp.swift
@@ -0,0 +1,18 @@
+//
+//  ChessGameApp.swift
+//  chess_project_IOS
+//
+//  Created by NYCDOE on 3/30/26.
+//
+
+import SwiftUI
+
+struct ChessGameApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChessGameApp()
+}
